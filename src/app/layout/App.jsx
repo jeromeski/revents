f0858137f4d731f13483b191dd0d81964d87f792
Jeromeski <jeromegee@gmail.com>
2019-12-30 08:15:23 +0800

    added EventDashboard

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Revents</h1>
+        <EventDashboard/>
       </div>
     );
   }
