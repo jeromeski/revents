7eaf6eda08f4c702614ad0316afe261ad8e28dfe
Jeromeski <jeromegee@gmail.com>
2020-02-03 19:35:30 +0800

    Created authReducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,11 +3,13 @@ import { reducer as FormReducer } from 'redux-form';
 import testReducer from "../features/test-area/testReducer";
 import eventReducer from "./events/eventReducer";
 import modalReducer from "../features/modals/modalReducer";
+import authReducer from "../features/auth/authReducer";
 
 
 export default combineReducers({
   form: FormReducer,
   test: testReducer,
   events: eventReducer,
-  modals: modalReducer
+  modals: modalReducer,
+  auth: authReducer
 });
